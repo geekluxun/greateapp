93c4673efeb2dd30e64fa2f7d5544be0115c8d89
geekluxun <geekluxun@163.com>
2017-08-31 20:43:10 +0800

    Initial commit

---
+++ src/main/java/com/geekluxun/greateapp/GreateappApplication.java
@@ -0,0 +1,17 @@
+package com.geekluxun.greateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GreateappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreateappApplication.class, args);
+	}
+
+
+
+
+}
---
+++ src/test/java/com/geekluxun/greateapp/GreateappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.geekluxun.greateapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GreateappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
