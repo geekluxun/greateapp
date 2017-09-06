a758fac19aa18b23f258d8fc4f774f03f5a919f6
geekluxun <geekluxun@163.com>
2017-09-06 23:08:56 +0800

    增加测试代码

--- src/test/java/com/geekluxun/greateapp/GreateappApplicationTests.java
+++ src/test/java/com/geekluxun/greateapp/GreateappApplicationTests.java
@@ -1,9 +1,15 @@
 package com.geekluxun.greateapp;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.geekluxun.greateapp.entity.TUser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +17,28 @@ public class GreateappApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testJson(){
+		List list = new ArrayList();
+		TUser user = new TUser();
+		user.setId(11L);
+		user.setName("luxun");
+		user.setPassword("123");
+		list.add(user);
+
+		user = new TUser();
+		user.setId(22L);
+		user.setName("luxun");
+		user.setPassword("456");
+		list.add(user);
+
+		String s = JSON.toJSONString(list);
+
+		list = JSON.parseObject(s, new TypeReference<List<TUser>>() {});
+
+		System.out.println(list.toString());
 	}
 
 }
