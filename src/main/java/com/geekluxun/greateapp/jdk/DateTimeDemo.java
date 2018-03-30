c90455b9abc5d43042c88f8bd5c32b3a2585d808
NAMIBANK\luxun <luxun@namibank.com>
2018-03-30 18:50:50 +0800

    Calendar测试

--- src/main/java/com/geekluxun/greateapp/jdk/DateTimeDemo.java
+++ src/main/java/com/geekluxun/greateapp/jdk/DateTimeDemo.java
@@ -1,5 +1,9 @@
 package com.geekluxun.greateapp.jdk;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -11,6 +15,7 @@ public class DateTimeDemo {
     public static void main(String[] argc){
         DateTimeDemo dateTimeDemo = new DateTimeDemo();
         dateTimeDemo.testCalendar();
+        dateTimeDemo.testCalendar2();
     }
 
 
@@ -33,5 +38,26 @@ public class DateTimeDemo {
         int year = now.get(Calendar.YEAR);
         int maxMonth = now.getMaximum(Calendar.MONTH);
         System.out.println();
+    }
+
+    private void testCalendar2(){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+
+
+        Calendar calendar = Calendar.getInstance();
+        //month取值0-11
+        calendar.set(2018,01,1, 0, 0, 0);
+        System.out.println("时间" +  format.format(calendar.getTime()));
+        //当前月实际最大天数
+        int maxDayOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        int minDayOfMonth = calendar.getActualMinimum(Calendar.DAY_OF_MONTH);
+        int maxYear = calendar.getActualMaximum(Calendar.YEAR);
+        int maxHourOfDay = calendar.getActualMaximum(Calendar.HOUR_OF_DAY);
+        int maxHour = calendar.getActualMaximum(Calendar.HOUR);
+        //当前日期增加一个月 2018-01-31 增加一个月是2018-02-28
+        calendar.add(Calendar.MONTH, 1);
+        System.out.println("时间" +  format.format(calendar.getTime()));
+        System.out.println("当前月最小天:" + minDayOfMonth + "最大天:" + maxDayOfMonth);
+
     }
 }
