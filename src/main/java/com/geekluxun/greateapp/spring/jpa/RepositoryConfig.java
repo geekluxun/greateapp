412050c4ce4c56f1762fd0d0889fe45da83b7833
luxun <geekluxun@163.com>
2018-12-03 21:37:24 +0800

    jpa

---
+++ src/main/java/com/geekluxun/greateapp/spring/jpa/RepositoryConfig.java
@@ -0,0 +1,13 @@
+package com.geekluxun.greateapp.spring.jpa;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * @Author: luxun
+ * @Create: 2018-12-03 21:11
+ * @Description:
+ * @Other:
+ */
+@EnableJpaRepositories(basePackages = {"om.geekluxun.greateapp.spring.jpa"})
+public class RepositoryConfig {
+}
