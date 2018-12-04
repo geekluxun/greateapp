5e1ac2f4ace5972c2c8710133b5a390ea62fd385
geekluxun <geekluxun@163.com>
2018-12-04 16:28:18 +0800

    重构项目结构

--- spring-data/src/main/java/com/geekluxun/springdata/SpringDataApplication.java
+++ spring-data/src/main/java/com/geekluxun/springdata/SpringDataApplication.java
@@ -1,12 +1,12 @@
-package com.geekluxun.springdatajpa;
+package com.geekluxun.springdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataJpaApplication {
+public class SpringDataApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringDataJpaApplication.class, args);
+        SpringApplication.run(SpringDataApplication.class, args);
     }
 }
